f481389c12b9b0efe45ca93ea0659d9f71b19788
VasilGVasilev <66898804+VasilGVasilev@users.noreply.github.com>
2023-06-07 19:11:50 +0300

    dotsgroup adjustments

--- app/Home.js
+++ app/Home.js
@@ -14,46 +14,46 @@ import { useEffect, useState } from "react";
 import { motion } from "framer-motion";
 
 export default function Home() {
-    const [selectedPage, setSelectedPage] = useState("home"); //where are we navigationwise
-    const [isTopOfPage, setIsTopOfPage] = useState(true);
-    const isDesktop = useMediaQuery("(min-width: 1060px)");
-  
-    useEffect(() => {
-      const handleScroll = () => {
-        if (window.scrollY === 0) {
-          setIsTopOfPage(true);
-          setSelectedPage("home");
-        }
-        if (window.scrollY !== 0) setIsTopOfPage(false);
-      };
-      window.addEventListener("scroll", handleScroll);
-      return () => window.removeEventListener("scroll", handleScroll);
-    }, []);
-  
-    return (
-      <div className="bg-deep-blue">
-        {/* navbar is not in layout because layout cannot pass on props and context is too complex for the current app magnitude */}
-        <Navbar
-          isTopOfPage={isTopOfPage}
-          selectedPage={selectedPage}
-          setSelectedPage={setSelectedPage}
-        />
-        <div className="w-5/6 mx-auto md:h-full">
-          {isDesktop && (
-            <DotGroup
-              selectedPage={selectedPage}
-              setSelectedPage={setSelectedPage}
-            />
-          )}
-          <motion.div
-            margin="0 0 -200px 0"
-            amount="all"
-            onViewportEnter={() => setSelectedPage("home")}
-          >
-            {/* <Landing setSelectedPage={setSelectedPage} /> */}
-          </motion.div>
-        </div>
-        {/* <LineGradient />
+  const [selectedPage, setSelectedPage] = useState("home"); //where are we navigationwise
+  const [isTopOfPage, setIsTopOfPage] = useState(true);
+  const isDesktop = useMediaQuery("(min-width: 1060px)");
+
+  useEffect(() => {
+    const handleScroll = () => {
+      if (window.scrollY === 0) {
+        setIsTopOfPage(true);
+        setSelectedPage("home");
+      }
+      if (window.scrollY !== 0) setIsTopOfPage(false);
+    };
+    window.addEventListener("scroll", handleScroll);
+    return () => window.removeEventListener("scroll", handleScroll);
+  }, []);
+
+  return (
+    <div className="bg-deep-blue">
+      {/* navbar is not in layout because layout cannot pass on props and context is too complex for the current app magnitude */}
+      <Navbar
+        isTopOfPage={isTopOfPage}
+        selectedPage={selectedPage}
+        setSelectedPage={setSelectedPage}
+      />
+      <div className="w-5/6 mx-auto md:h-full">
+        {isDesktop && (
+          <DotGroup
+            selectedPage={selectedPage}
+            setSelectedPage={setSelectedPage}
+          />
+        )}
+        <motion.div
+          margin="0 0 -200px 0"
+          amount="all"
+          onViewportEnter={() => setSelectedPage("home")}
+        >
+          {/* <Landing setSelectedPage={setSelectedPage} /> */}
+        </motion.div>
+      </div>
+      {/* <LineGradient />
         <div className="w-5/6 mx-auto md:h-full ">
           <motion.div
             margin="0 0 -200px 0"
@@ -94,8 +94,7 @@ export default function Home() {
           </motion.div>
         </div>
         <Footer /> */}
-      </div>
-    );
-  }
-  
-  
+    </div>
+  );
+}
+
--- components/DotGroup.js
+++ components/DotGroup.js
@@ -1,48 +1,46 @@
 import Link from "next/link";
 
 const DotGroup = ({ selectedPage, setSelectedPage }) => {
+  // the ring around the selected menu 
   const selectedStyles = `relative bg-yellow before:absolute before:w-6 before:h-6 before:rounded-full
     before:border-2 before:border-yellow before:left-[-50%] before:top-[-50%]`;
+  
+  const notSelectedStyles = `bg-dark-grey hover:bg-yellow active:bg-yellow transition duration-200 hover:scale-110 active:scale-110 `
 
   return (
     <div className="flex flex-col gap-6 fixed top-[60%] right-7">
       <Link
         href="#home"
-        className={`${
-          selectedPage === "home" ? selectedStyles : "bg-dark-grey"
-        } w-3 h-3 rounded-full`}
+        className={`${selectedPage === "home" ? selectedStyles : notSelectedStyles
+          } w-3 h-3 rounded-full`}
         onClick={() => setSelectedPage("home")}
       />
 
       <Link
         href="#skills"
-        className={`${
-          selectedPage === "skills" ? selectedStyles : "bg-dark-grey"
-        } w-3 h-3 rounded-full`}
+        className={`${selectedPage === "skills" ? selectedStyles : notSelectedStyles
+          } w-3 h-3 rounded-full`}
         onClick={() => setSelectedPage("skills")}
       />
 
       <Link
         href="#projects"
-        className={`${
-          selectedPage === "projects" ? selectedStyles : "bg-dark-grey"
-        } w-3 h-3 rounded-full`}
+        className={`${selectedPage === "projects" ? selectedStyles : notSelectedStyles
+          } w-3 h-3 rounded-full`}
         onClick={() => setSelectedPage("projects")}
       />
 
       <Link
         href="#testimonials"
-        className={`${
-          selectedPage === "testimonials" ? selectedStyles : "bg-dark-grey"
-        } w-3 h-3 rounded-full`}
+        className={`${selectedPage === "testimonials" ? selectedStyles : notSelectedStyles
+          } w-3 h-3 rounded-full`}
         onClick={() => setSelectedPage("testimonials")}
       />
 
       <Link
         href="#contact"
-        className={`${
-          selectedPage === "contact" ? selectedStyles : "bg-dark-grey"
-        } w-3 h-3 rounded-full`}
+        className={`${selectedPage === "contact" ? selectedStyles : notSelectedStyles
+          } w-3 h-3 rounded-full`}
         onClick={() => setSelectedPage("contact")}
       />
     </div>
