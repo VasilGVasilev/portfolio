b831e159d7d1ff67e85d6d9f6c7e0705166f0041
VasilGVasilev <66898804+VasilGVasilev@users.noreply.github.com>
2023-06-07 12:11:11 +0300

    hook to check on screen size for potential functionality

--- app/hooks/useMediaQuery.jsx
+++ app/hooks/useMediaQuery.jsx
@@ -1,14 +1,22 @@
-// No need to use this hook due to the fact that Taiwlind comes with inline media queries
+// using this hook for dots refering to components, tailwind inline media query is not enough
+// on every resize we check if query param ("(min-width: 1060px)") matches the current size of desktop and if yes, we return true which as a Boolean has significance in Home.js
 
-import { useState, useEffect } from 'react';
+import { useState, useEffect } from "react";
 
 const useMediaQuery = (query) => {
-    const [matches, setMatches] = useState(false);
+  const [matches, setMatches] = useState(false);
 
-    useEffect(()=>{
-        const media = window.matchMedia(query); //returns a new MediaQueryList object (instance props - .matches and .media) that can then be used to determine if the document matches the media query string
-        if (media.matches !== matches){
-            setMatches(media.matches);
-        }
-    })
-}
+  useEffect(() => {
+    const media = window.matchMedia(query);
+    if (media.matches !== matches) {
+      setMatches(media.matches);
+    }
+    const listener = () => setMatches(media.matches);
+    window.addEventListener("resize", listener);
+    return () => window.removeEventListener("resize", listener); //cleanup function
+  }, [matches, query]);
+
+  return matches;
+};
+
+export default useMediaQuery;
