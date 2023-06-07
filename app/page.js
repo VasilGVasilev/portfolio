68cb32be5aa79c90a87f1f53212a7b229c1b8655
VasilGVasilev <66898804+VasilGVasilev@users.noreply.github.com>
2023-06-07 12:11:54 +0300

    abstract server component from client component

--- app/page.js
+++ app/page.js
@@ -1,14 +1,12 @@
+import Home from "./Home";
 
+export const metadata = {
+  title: 'Vasil Vasilev\'s portfolio',
+  description: 'Vasil Vasilev\'s portfolio website'
+}
 
 export default function Page() {
   return (
-<>
-<div className="text-2xl">I’m a Web Developer based in Adelaide, Australia. Even though I have a Telecommunications Engineering degree and an MBA, coding is my passion and my new professional career focus. Developing applications for the web has become my drive.</div>
-<div className="text-2xl">I’m a Web Developer based in Adelaide, Australia. Even though I have a Telecommunications Engineering degree and an MBA, coding is my passion and my new professional career focus. Developing applications for the web has become my drive.</div>
-<div className="text-2xl">I’m a Web Developer based in Adelaide, Australia. Even though I have a Telecommunications Engineering degree and an MBA, coding is my passion and my new professional career focus. Developing applications for the web has become my drive.</div>
-<div className="text-2xl">I’m a Web Developer based in Adelaide, Australia. Even though I have a Telecommunications Engineering degree and an MBA, coding is my passion and my new professional career focus. Developing applications for the web has become my drive.</div>
-
-<div className="text-2xl">I’m a Web Developer based in Adelaide, Australia. Even though I have a Telecommunications Engineering degree and an MBA, coding is my passion and my new professional career focus. Developing applications for the web has become my drive.</div>
-</>
+    <Home></Home>
   )
 }
