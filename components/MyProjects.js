c71f7f8f1b1e6cc8bbd5240ece7e38af271a61fe
VasilGVasilev <66898804+VasilGVasilev@users.noreply.github.com>
2023-06-15 12:49:46 +0300

    better projects styling

--- components/MyProjects.js
+++ components/MyProjects.js
@@ -4,6 +4,53 @@ import { motion } from "framer-motion";
 import { playfair } from "@/app/fonts";
 import Link from "next/link";
 
+const container = {
+  hidden: {},
+  visible: {
+    transition: {
+      staggerChildren: 0.2,
+    },
+  },
+};
+
+const Project = ({ number, color, url, altLink, srcImg, projectName, projectDesc }) => {
+  const overlayStyles = `md:w-1/3 mt-10 relative z-0 ml-20 p-16 before:absolute before:bottom-10 before:right-10
+  before:w-full before:h-full before:border-2 before:border-${color} before:z-[-1]`;
+
+  return (
+      <motion.div
+      className={overlayStyles}
+      initial="hidden"
+      whileInView="visible"
+      viewport={{ once: true, amount: 0.5 }}
+      transition={{ delay: 0.4, duration: 0.5 }}
+      variants={{
+        hidden: { opacity: 0, y: 50 },
+        visible: { opacity: 1, y: 0 },
+      }}
+    >
+      <div className="relative h-32">
+        <div className="z-10 absolute">
+          <p className={`${playfair.className} font-semibold text-5xl`}>0{number}</p>
+          <p className={`${playfair.className} font-semibold text-3xl mt-3`}>
+            {projectName}
+          </p>
+        </div>
+        <div className={`w-1/2 md:w-3/4 h-32 bg-${color} absolute right-0 top-0 z-[1]`} />
+      </div>
+      <p className="mt-5">
+          {projectDesc}
+      </p>
+      <div className={`w-full h-fit m-5 overflow-hidden mx-auto border-${color} border-t-4 border-r-4`}>
+        {/* The CSS OVERFLOW property controls what happens to content that is too big to fit into an area. */}
+        <Link href={url} >
+          <img src={srcImg} alt={altLink} className="transition-transform duration-700 ease-in-out hover:scale-150" />
+        </Link>
+      </div>
+    </motion.div>
+  );
+};
+
 const MyProjects = () => {
   const isAboveLarge = useMediaQuery("(min-width: 1060px)");
   return (
@@ -54,112 +101,40 @@ const MyProjects = () => {
       {/* PROJECTS */}
       <div className="md:flex md:justify-between mt-16 gap-16">
 
+
         {/* IXORA */}
-        <motion.div
-          className="md:w-1/3 mt-10 relative z-0 ml-20 p-16 before:absolute before:bottom-10 before:right-10
-          before:w-full before:h-full before:border-2 before:border-blue before:z-[-1]"
-          initial="hidden"
-          whileInView="visible"
-          viewport={{ once: true, amount: 0.5 }}
-          transition={{ duration: 0.5 }}
-          variants={{
-            hidden: { opacity: 0, y: 50 },
-            visible: { opacity: 1, y: 0 },
-          }}
-        >
-          <div className="relative h-32">
-            <div className="z-10 absolute">
-              <p className={`${playfair.className}  font-semibold text-5xl`}>01</p>
-              <p className={`${playfair.className}  font-semibold text-3xl mt-3`}>
-                Ixora Residence
-              </p>
-            </div>
-            <div className="w-1/2 md:w-3/4 h-32 bg-blue absolute right-0 top-0 z-[1]" />
-          </div>
-          <p className="mt-5">
-              A SSG website for a freelance project developed via Next.js, Tailwind, React.
-               The main feature is change of color signifying the apartment status - sold, available, reserved.
-          </p>
-          <div className="w-full h-full m-5 overflow-hidden mx-auto">
-            {/* The CSS OVERFLOW property controls what happens to content that is too big to fit into an area. */}
-            <Link href={"https://ixorabg.com/"} >
-              <img src="assets/project-one.webp" alt="ixroa-img" className=" transition-transform duration-500 ease-in-out hover:scale-125" />
-            </Link>
-          </div>
-          
-        </motion.div>
+        <Project
+          number='1' 
+          color="blue" 
+          url="https://ixorabg.com/" 
+          altLink="ixroa-img" 
+          srcImg="assets/project-one.webp" 
+          projectName={'Ixora Residence'} 
+          projectDesc={'A SSG website for a freelance project developed via Next.js, Tailwind, React. The main feature is change of color signifying the apartment status - sold, available, reserved.'} 
+        />
 
-        {/* INNOVATIVE */}
-        <motion.div
-          className="md:w-1/3 mt-10 relative z-0 ml-20 p-16 before:absolute before:bottom-10 before:right-10
-          before:w-full before:h-full before:border-2 before:border-red before:z-[-1]"
-          initial="hidden"
-          whileInView="visible"
-          viewport={{ once: true, amount: 0.5 }}
-          transition={{ delay: 0.2, duration: 0.5 }}
-          variants={{
-            hidden: { opacity: 0, y: 50 },
-            visible: { opacity: 1, y: 0 },
-          }}
-        >
-          <div className="relative h-32">
-            <div className="z-10 absolute">
-              <p className={`${playfair.className}  font-semibold text-5xl`}>02</p>
-              <p className={`${playfair.className}  font-semibold text-3xl mt-3`}>
-                Innovative
-              </p>
-            </div>
-            <div className="w-1/2 md:w-3/4 h-32 bg-red absolute right-0 top-0 z-[1]" />
-          </div>
-          <p className="mt-5">
-              Urna, eget pulvinar dolor cursus volutpat dictum odio. Nec ultricies
-              amet in in neque nibh tortor. Libero sed pretium justo nulla blandit
-              nulla amet habitant iaculis. Iaculis in congue vitae sollicitudin
-              faucibus a
-          </p>
-          <div className="w-full h-full m-5 overflow-hidden mx-auto">
-            {/* The CSS OVERFLOW property controls what happens to content that is too big to fit into an area. */}
-            <Link href={"https://ixorabg.com/"} >
-              <img src="assets/project-one.webp" alt="ixroa-img" className=" transition-transform duration-500 ease-in-out hover:scale-125" />
-            </Link>
-          </div>
-        </motion.div>
+        {/* OTHER */}
+        <Project 
+          number='2' 
+          color="red" 
+          url="https://ixorabg.com/" 
+          altLink="ixroa-img" 
+          srcImg="assets/project-one.webp" 
+          projectName={'Ixora Residence'} 
+          projectDesc={'A SSG website for a freelance project developed via Next.js, Tailwind, React. The main feature is change of color signifying the apartment status - sold, available, reserved.'} 
+        />
 
-        {/* IMAGINATIVE */}
-        <motion.div
-          className="md:w-1/3 mt-10 relative z-0 ml-20 p-16 before:absolute before:bottom-10 before:right-10
-          before:w-full before:h-full before:border-2 before:border-red before:z-[-1]"
-          initial="hidden"
-          whileInView="visible"
-          viewport={{ once: true, amount: 0.5 }}
-          transition={{ delay: 0.4, duration: 0.5 }}
-          variants={{
-            hidden: { opacity: 0, y: 50 },
-            visible: { opacity: 1, y: 0 },
-          }}
-        >
-          <div className="relative h-32">
-            <div className="z-10 absolute">
-              <p className={`${playfair.className} font-semibold text-5xl`}>03</p>
-              <p className={`${playfair.className} font-semibold text-3xl mt-3`}>
-                Imaginative
-              </p>
-            </div>
-            <div className="w-1/2 md:w-3/4 h-32 bg-yellow absolute right-0 top-0 z-[1]" />
-          </div>
-          <p className="mt-5">
-              Accumsan eu fringilla nisi, eget. Vitae, eget ut id proin arcu in
-              curabitur. Lectus libero, egestas enim aliquam quis felis amet.
-              Sagittis, amet netus fringilla netus lobortis odio sed platea.
-              Bibendum.
-          </p>
-          <div className="w-full h-full m-5 overflow-hidden mx-auto">
-            {/* The CSS OVERFLOW property controls what happens to content that is too big to fit into an area. */}
-            <Link href={"https://ixorabg.com/"} >
-              <img src="assets/project-one.webp" alt="ixroa-img" className=" transition-transform duration-500 ease-in-out hover:scale-125" />
-            </Link>
-          </div>
-        </motion.div>
+        {/* OTHER */}
+        <Project 
+          number='3' 
+          color="yellow" 
+          url="https://ixorabg.com/" 
+          altLink="ixroa-img" 
+          srcImg="assets/project-one.webp" 
+          projectName={'Ixora Residence'} 
+          projectDesc={'A SSG website for a freelance project developed via Next.js, Tailwind, React. The main feature is change of color signifying the apartment status - sold, available, reserved.'} 
+        />
+
       </div>
     </section>
   );
