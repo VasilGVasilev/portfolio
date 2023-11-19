97f5b71ee16ac8949a51f1bc57316bbb21b44ca7
VasilGVasilev <66898804+VasilGVasilev@users.noreply.github.com>
2023-11-19 16:13:54 +0200

    mobile nav better functionality
    mobile modal's bg div has opacity when clicked, also clicking a link navigates to it

--- components/Navbar.js
+++ components/Navbar.js
@@ -1,9 +1,9 @@
-import { useState } from "react";
+import { useRef, useState } from "react";
 import useMediaQuery from "@/app/hooks/useMediaQuery";
 import { opensans, playfair } from "@/app/utils/fonts";
-import AnchorLink from 'react-anchor-link-smooth-scroll'
 import { AiOutlineClose, AiOutlineMenu } from 'react-icons/ai';
-
+import Link from "next/link";
+import { motion } from "framer-motion";
 
 // props: 
 // - name of link, must be constant
@@ -12,20 +12,24 @@ import { AiOutlineClose, AiOutlineMenu } from 'react-icons/ai';
 
 // TODO: 
 // mobile menu comes as an animation with framer motion
-// may be reverse link to anchor link if not smooth
 // refactor img to Image
-const LinkTemplate = ({ page, selectedPage, setSelectedPage }) => {
+const LinkTemplate = ({ page, selectedPage, setSelectedPage, setIsMenuToggled, isMenuToggled }) => {
   const lowerCasePage = page.toLowerCase();
+  const exitModalToSelectedPage = (lowerCasePage) => {
+    setSelectedPage(lowerCasePage)
+    if (setIsMenuToggled) {
+      setIsMenuToggled(!isMenuToggled)
+    }
+  }
   return (
-    <AnchorLink
-      className={`${
-        selectedPage === lowerCasePage ? "text-[#facc15]" : ""
-      }  hover:scale-125 transition duration-300`}
+    <Link
+      className={`${selectedPage === lowerCasePage ? "text-[#facc15]" : ""
+        }  hover:scale-125 transition duration-300`}
       href={`#${lowerCasePage}`}
-      onClick={() => setSelectedPage(lowerCasePage)}
+      onClick={() => exitModalToSelectedPage(lowerCasePage)}
     >
       {page}
-    </AnchorLink>
+    </Link>
   );
 };
 
@@ -33,11 +37,19 @@ const Navbar = ({ isTopOfPage, selectedPage, setSelectedPage }) => {
   const [isMenuToggled, setIsMenuToggled] = useState(false);
   const isDesktop = useMediaQuery("(min-width: 768px)");
   const navbarBackground = isTopOfPage ? "" : "bg-blue";
+  const modalRef = useRef(null);
+
+  const closeModal = (e) => {
+    e.stopPropagation();
+    if (e.target == modalRef.current) {
+      setIsMenuToggled(!isMenuToggled)
+    }
+  }
 
   return (
     <nav className={`${navbarBackground} z-40 w-full fixed top-0 py-6`}>
       <div className="flex items-center justify-between mx-auto w-5/6">
-        <AnchorLink href={'#home'} ><h4 className={`${playfair.className} text-3xl font-bold`}>Vasil Vasilev</h4></AnchorLink> 
+        <Link href={'#home'} ><h4 className={`${playfair.className} text-3xl font-bold`}>Vasil Vasilev</h4></Link>
 
         {/* DESKTOP NAV */}
         {isDesktop ? (
@@ -80,45 +92,71 @@ const Navbar = ({ isTopOfPage, selectedPage, setSelectedPage }) => {
 
         {/* MOBILE MENU POPUP */}
         {!isDesktop && isMenuToggled && (
-          <div className="fixed right-0 bottom-0 h-full bg-blue w-[300px]">
-            {/* CLOSE ICON */}
-            <div className="flex justify-end p-12">
-              <button 
-                onClick={() => setIsMenuToggled(!isMenuToggled)}
-                aria-label="Close Mobile Menu"
-              >
-                <AiOutlineClose size={24} />
-              </button>
-            </div>
+          <div
+            className="fixed inset-0 z-40 flex items-center justify-center bg-black bg-opacity-80"
+            onClick={closeModal}
+            ref={modalRef}
+          >
+            <motion.div
+              className="fixed right-0 bottom-0 h-full bg-blue w-[300px]"
+              initial="hidden"
+              whileInView="visible"
+              viewport={{ once: true }}
+              transition={{ duration: 0.3 }}
+              variants={{
+                hidden: { opacity: 0, x: 50 },
+                visible: { opacity: 1, x: 0 },
+              }}
+            >
+              {/* CLOSE ICON */}
+              <div className="flex justify-end p-12">
+                <button
+                  onClick={() => setIsMenuToggled(!isMenuToggled)}
+                  aria-label="Close Mobile Menu"
+                >
+                  <AiOutlineClose size={24} />
+                </button>
+              </div>
 
-            {/* MENU ITEMS */}
-            <div className="flex flex-col gap-10 ml-[33%] text-2xl text-deep-blue">
-              <LinkTemplate
-                page="Home"
-                selectedPage={selectedPage}
-                setSelectedPage={setSelectedPage}
-              />
-              <LinkTemplate
-                page="Projects"
-                selectedPage={selectedPage}
-                setSelectedPage={setSelectedPage}
-              />
-              <LinkTemplate
-                page="Articles"
-                selectedPage={selectedPage}
-                setSelectedPage={setSelectedPage}
-              />
-              <LinkTemplate
-                page="Qualifications"
-                selectedPage={selectedPage}
-                setSelectedPage={setSelectedPage}
-              />
-              <LinkTemplate
-                page="Contact"
-                selectedPage={selectedPage}
-                setSelectedPage={setSelectedPage}
-              />
-            </div>
+              {/* MENU ITEMS */}
+              <div className="flex flex-col gap-10 ml-[33%] text-2xl text-deep-blue">
+                <LinkTemplate
+                  page="Home"
+                  selectedPage={selectedPage}
+                  setSelectedPage={setSelectedPage}
+                  setIsMenuToggled={setIsMenuToggled}
+                  isMenuToggled={isMenuToggled}
+                />
+                <LinkTemplate
+                  page="Projects"
+                  selectedPage={selectedPage}
+                  setSelectedPage={setSelectedPage}
+                  setIsMenuToggled={setIsMenuToggled}
+                  isMenuToggled={isMenuToggled}
+                />
+                <LinkTemplate
+                  page="Articles"
+                  selectedPage={selectedPage}
+                  setSelectedPage={setSelectedPage}
+                  setIsMenuToggled={setIsMenuToggled}
+                  isMenuToggled={isMenuToggled}
+                />
+                <LinkTemplate
+                  page="Qualifications"
+                  selectedPage={selectedPage}
+                  setSelectedPage={setSelectedPage}
+                  setIsMenuToggled={setIsMenuToggled}
+                  isMenuToggled={isMenuToggled}
+                />
+                <LinkTemplate
+                  page="Contact"
+                  selectedPage={selectedPage}
+                  setSelectedPage={setSelectedPage}
+                  setIsMenuToggled={setIsMenuToggled}
+                  isMenuToggled={isMenuToggled}
+                />
+              </div>
+            </motion.div>
           </div>
         )}
       </div>
